99cf3f3f73d22425941105a2798779cb6f363df4
恒锋 姚 <TINGFENG20@outlook.com>
2019-04-15 20:29:14 +0800

    添加项目文件。

---
+++ OpenCV_01/Program.cs
@@ -0,0 +1,17 @@
+﻿using OpenCvSharp;
+using System;
+
+namespace OpenCV_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Mat m = new Mat(@"C:\Users\Administrator\Desktop\头像\a.jpg", ImreadModes.Color);
+            Cv2.ImShow("Demo",m);
+            Cv2.WaitKey(0);
+            Console.ReadKey();
+        }
+    }
+}
